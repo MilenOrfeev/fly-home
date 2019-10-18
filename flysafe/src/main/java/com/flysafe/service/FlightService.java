6e05c32131669f8f2fb2ace8da835c5de9e187b1
MilenOrfeev <milen.orfeev@abv.bg>
2019-10-18 23:14:31 +0100

    Add Service

---
+++ flysafe/src/main/java/com/flysafe/service/FlightService.java
@@ -0,0 +1,13 @@
+package com.flysafe.service;
+
+import com.flysafe.model.Flight;
+import com.flysafe.model.FlightRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FlightService {
+    public Flight findFlights(FlightRequest flightRequest) {
+        return null;
+    }
+
+}
