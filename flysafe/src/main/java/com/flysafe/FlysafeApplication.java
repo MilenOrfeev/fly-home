c168cfefe107b511debd58f0f35605753864055b
MilenOrfeev <milen.orfeev@abv.bg>
2019-10-18 22:15:21 +0100

    Create Spring back-end

---
+++ flysafe/src/main/java/com/flysafe/FlysafeApplication.java
@@ -0,0 +1,13 @@
+package com.flysafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlysafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlysafeApplication.class, args);
+	}
+
+}
---
+++ flysafe/src/test/java/com/flysafe/FlysafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flysafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlysafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
