20c2958a9ff04564fea721aa1e841b3528b2f413
MilenOrfeev <milen.orfeev@abv.bg>
2019-10-19 08:01:59 +0100

    Annotate with Lombok

--- flysafe/src/main/java/com/flysafe/skyscanner/FlightResponse.java
+++ flysafe/src/main/java/com/flysafe/skyscanner/FlightResponse.java
@@ -1,9 +1,11 @@
 package com.flysafe.skyscanner;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class FlightResponse {
     @JsonProperty("Quotes")
     private List<Quote> quotes;
